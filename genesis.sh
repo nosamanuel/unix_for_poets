f5c8ac1212a1bb739e9cfb8c1a5440f2d03ef4e6
Noah Seger <nosamanuel@gmail.com>
2012-03-16 08:43:09 -0500

    Finished page 17

--- genesis.sh
+++ genesis.sh
@@ -22,3 +22,37 @@ sort -f genesis.bigrams.txt | uniq -ci | sort -nr | head -n 20
 
 # Top trigrams
 sort -f genesis.trigrams.txt | uniq -ci | sort -nr | head -n 20
+
+# Count "-ing" words
+grep -e "ing$" genesis.words.txt | wc -l
+
+# Count uppercase words
+grep -E "^[A-Z]+$" genesis.words.txt | wc -l
+
+# Count 4-letter words
+grep -E "^.{4}$" genesis.words.txt | wc -l
+
+# Find words without vowels
+grep -Ei "^[^aeiou]{2,}$" genesis.words.txt | sort -f | uniq -i
+
+# Find words with exactly 1 vowel ("1-syllable" words)
+grep -Ei "^[^aeiouy]*[aeiouy][^aeiouy]*$" genesis.words.txt | sort -f | uniq -i
+
+# Find words with exactly 2 vowels ("2-syllable" words)
+grep -Ei "^[^aeiouy]*[aeiouy][^aeiouy]*[aeiouy][^aeiouy]*$" genesis.words.txt | sort -f | uniq -i > genesis.2syllables.txt
+
+# Delete words ending with silent "e" or containing dipthongs
+grep -Eiv "(ow|ou|ie|oi|oo|ea|ee|ai|[aeiou]y).*|[aeiouy][^aeiouy]e$" genesis.2syllables.txt
+
+# Find verses with the word "light"
+grep -Ei "^.*\blight\b.*$" genesis.txt
+
+# Count verses with two or more instances of "light"
+grep -Ei "^(.*light.*){2,}$" genesis.txt | wc -l
+
+# Count verses with three or more instances of "light"
+grep -Ei "^(.*light.*){3,}$" genesis.txt | wc -l
+
+# Count verses with exactly two instances of "light"
+grep -Ei "^(.*light.*){2,}$" genesis.txt | grep -Eiv "^(.*light.*){3,}$" | wc -l
+grep -P '^((.(?!light))*(.light)(.(?!light))*){2}$' genesis.txt | wc -l
